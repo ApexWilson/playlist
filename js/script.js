31bd6c8ea769deeb2f3180e617de60a7b0cda148
ApexWilson <wongwilson112@gmail.com>
2020-02-19 02:26:31 +0000

    ew objects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+var imageList=['https://www.readdork.com/images/article/Artist-Images/H/Hayley%20Williams/Simmer-Single-Art.jpg', 'https://i.ytimg.com/vi/-uj9b9JCIJM/maxresdefault.jpg','https://i.ytimg.com/vi/4SZEDBFPpgw/maxresdefault.jpg'];
+imageList.forEach(function(images){
+    $('.images').append("<p>" + `<img src=${images}>` + "</p>");
+});
 var artistList=['Hayley Williams', 'The Weeknd','Porter Robinson'];
 artistList.forEach(function(artists){
-    $('.artistList').append("<p>" + "By:"+ artists + "</p>");
+    $('.artistList').append("<p>" + "By: "+ artists + "</p>");
 });
 var lengthList=['4:47','4:03','3:38'];
 lengthList.forEach(function(lengths){
@@ -12,5 +16,15 @@ songList.forEach(function(SongNames){
 });
 var linkList=['https://www.youtube.com/watch?v=93MpOKKcjL0','https://www.youtube.com/watch?v=1DpH-icPpl0','https://www.youtube.com/watch?v=4SZEDBFPpgw'];
 linkList.forEach(function(links){
-    $('.links').append("<p>" + links + "</p>");
+    $('.links').append("<p>" + `<a href=${links}>Listen</a>` + "</p>");
 });
+//var playList = [{image:'https://www.readdork.com/images/article/Artist-Images/H/Hayley%20Williams/Simmer-Single-Art.jpg'},{image:'https://i.ytimg.com/vi/-uj9b9JCIJM/maxresdefault.jpg'},
+//{image:'https://i.ytimg.com/vi/4SZEDBFPpgw/maxresdefault.jpg'},{artist:'Hayley Williams'},{artist:'The Weeknd'},{artist:'Porter Robinson'},{length:'4:47'},{length:'4:03'},{length:'3:38'},
+//{song:'Simmer Down'},{song:'Heartless'},{song:'Get your Wish'},{link:'https://www.youtube.com/watch?v=93MpOKKcjL0'},{link:'https://www.youtube.com/watch?v=1DpH-icPpl0'},{link:'https://www.youtube.com/watch?v=4SZEDBFPpgw'}];
+//playList.forEach(function(play){
+    //$('.images').append(play.image);
+    //$('.artistList').append(play.artist);
+    //$('.lengthList').append(play.length);
+    //$('.songList').append(play.song);
+    //$('.links').append(play.link);
+//});
